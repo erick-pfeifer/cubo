d8e8044587d0b2ab0474ec3591d7411b19cb5858
Erick Pfeifer <erick.pfeifer@gmail.com>
2025-05-15 10:34:49 -0500

    presubmit: Format code

--- tools/presubmit/py/presubmit.py
+++ tools/presubmit/py/presubmit.py
@@ -3,41 +3,44 @@ import logging
 import argparse
 
 import pw_cli.log
-from pw_presubmit import (git_repo, install_hook)
+from pw_presubmit import git_repo, install_hook
 
 __LOG = logging.getLogger(__name__)
 
-PRE_PUSH_HOOK_COMMANDS = ['bazelisk',
-                          'run',
-                          '@pigweed//pw_presubmit/py:format',
-                          '--',
-                          '--base',
-                          'HEAD~1',
-                          '--check']
+PRE_PUSH_HOOK_COMMANDS = [
+    "bazelisk",
+    "run",
+    "@pigweed//pw_presubmit/py:format",
+    "--",
+    "--base",
+    "HEAD~1",
+    "--check",
+]
 
 
 def run(install: bool) -> int:
     """Runs presubmit checks. Installs git hook if necessary."""
     if install:
-        install_hook.install_git_hook('pre-push',
-                                      PRE_PUSH_HOOK_COMMANDS,
-                                      git_repo.root())
-        __LOG.info('Presubmit checks are now installed as a git hook.')
+        install_hook.install_git_hook(
+            "pre-push", PRE_PUSH_HOOK_COMMANDS, git_repo.root()
+        )
+        __LOG.info("Presubmit checks are now installed as a git hook.")
         return 0
-
 
     return 0
 
 
 def main() -> int:
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--install',
-                        action='store_true',
-                        help='Install git hook for pre push operation.')
+    parser.add_argument(
+        "--install",
+        action="store_true",
+        help="Install git hook for pre push operation.",
+    )
 
     return run(**vars(parser.parse_args()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pw_cli.log.install(logging.INFO)
     sys.exit(main())
